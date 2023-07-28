59d87408dd5a71845811c0af717c3283f4c89738
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-28 16:44:52 +0530

    added 2 more utility to the tool

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,21 +15,36 @@ export default function TextForm(props) {
 
   return (
     <>
-      <h2>{props.heading}</h2>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="20"
-        ></textarea>
+      <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="15"
+          ></textarea>
+        </div>
+        <div className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Upper Case
+        </div>
+        {/* Lower Case */}
+        <div className="btn btn-primary mx-2">Convert to Lower Case</div>
       </div>
-      <div className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Upper Case
+
+      <div className="container my-4">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        {/* Speed of reading that paragraph */}
+        <p>{0.008 * text.split(" ").length} minutes to be read</p>
+
+        {/* Preview */}
+        <h2>Preview</h2>
+        <p>{text}</p>
       </div>
-      {/* Lower Case */}
-      <div className="btn btn-primary mx-2">Convert to Lower Case</div>
     </>
   );
 }
