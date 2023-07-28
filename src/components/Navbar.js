73e3829740dc4a558e613ad51851eccf4568c5f8
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-28 19:44:04 +0530

    Removed the search feature :(

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,17 +35,6 @@ export default function Navbar(props) {
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-primary" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
