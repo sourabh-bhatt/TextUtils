029fd3ad1ac12c54c81b019c7ff530eeba7e904f
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-29 14:23:04 +0530

    Added the routing into pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+// import { Link, Route, Routes } from "react-router-dom";
+// import Home from "./components/Home";
+// import BookList from "./components/Navbar";
+
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+
+//         <Route path="/Navbar" element={<Navbar />} />
+//         <Route path="/TextForm" element={<TextForm />} />
+//       </Routes>
+//     </>
+//   );
+// }
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About Us" />
-      {/* <Navbar /> */}
       <div className="container my-3 ">
+        <About />
         <TextForm heading="Enter the text to analyze below" />
       </div>
     </>
--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,21 @@ import React from "react";
 export default function About() {
   return (
     <>
-      <h1>This is About Page</h1>
+      {/* <h1>This is About Page</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+        numquam repellendus adipisci, deserunt, facilis corporis repellat, non
+        culpa optio itaque impedit esse. Error, exercitationem ab commodi
+        corrupti, officia cum laudantium reprehenderit, eius voluptatem
+        inventore beatae! Autem eaque totam atque necessitatibus, nam molestias
+        officiis quam labore aperiam iste doloremque, reiciendis, ab sunt.
+        Similique deserunt labore, quaerat soluta sint, numquam libero tempore
+        blanditiis eos molestiae ullam earum. Et cumque modi nam, velit nulla
+        provident voluptates error harum accusantium fuga at debitis rerum,
+        ipsum nesciunt ex doloremque consectetur sapiente nemo cupiditate dicta
+        sunt id consequuntur consequatur. Eaque autem explicabo nam dolor
+        officiis culpa!
+      </div> */}
     </>
   );
 }
